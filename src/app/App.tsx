9fabffd26817e88041ed178ee7cdee0933df0ead
FAST <krinickij.v.a@gmail.com>
2024-10-19 12:37:45 +0300

    fix : toggler error
    Тоглер нерабортал ( не переключался)

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -39,6 +39,7 @@ function App() {
                   <Toggler
                     name="toggler"
                     title="Toggler"
+                    value={true}
                   />
                 </form>
               </div>
--- src/shared/ui/form/Toggler.tsx
+++ src/shared/ui/form/Toggler.tsx
@@ -1,4 +1,4 @@
-import { FC } from "react"
+import { FC, useState } from "react"
 
 interface TogglerProps {
     title: string,
@@ -6,14 +6,20 @@ interface TogglerProps {
     value?: boolean,
 }
 export const Toggler: FC<TogglerProps> = ({ name, title, value=false }) => {
+    const [checkboxState, setCheckboxState] = useState(value)
+    
+    const changeState = ()=>{
+        setCheckboxState(!checkboxState)
+    }
     return (
-        <label className="inline-flex items-center cursor-pointer">
+        <label className="inline-flex items-center cursor-pointer" >
             <input
                 type="checkbox"
                 name={name}
                 value=""
                 className="sr-only peer"
-                checked={value}
+                checked={checkboxState}
+                onClick={changeState}
             />
             <div className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
             <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">{title}</span>
