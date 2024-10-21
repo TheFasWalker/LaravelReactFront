38b1059cadbb3a059b0fb1a810ff73564d04a993
krinickij <krinickij.v.a@gmail.com>
2024-10-21 19:52:10 -0400

    feat: edited logging form
    Переписана форма вхорда.
    Использован формик для формы входа

--- src/pages/Admin/Login.tsx
+++ src/pages/Admin/Login.tsx
@@ -1,58 +1,76 @@
-import { useState } from "react"
-import { BlueButton } from "../../shared/ui/elements/BlueBurron"
-import { InputField } from "../../shared/ui/form/InputField"
-import { Loader } from "../../shared/ui/elements/Loader"
-import { LoaderResult } from "../../shared/ui/elements/LoaderResult"
-import { useNavigate } from "react-router-dom"
 
+import { BlueButton } from "../../shared/ui/elements/BlueBurron";
+import { Loader } from "../../shared/ui/elements/Loader";
+import { Field, Form, Formik } from "formik";
+import { validateField } from "../../shared/helpers/validation";
+import { useState } from "react";
 
-
-export const Login =()=>{
-    const [loading, setLoading] = useState(false)
-    const [formData, setFormData] = useState({ email: "", password: "" });
-    const [error, setError] = useState("");
-    const navigate = useNavigate();
-    const handleChange = (e: { target: { name: string; value: string } }) => {
-        const { name, value } = e.target;
-        setFormData({ ...formData, [name]: value });
-    };
-    const submit = async (e: React.FormEvent<HTMLFormElement>)=>{
-        e.preventDefault();
-        setLoading(true)
-        setError("");
-        try {
-            const response = await fetch ('http://localhost:80/api/sanctum/auth',{
-                method:"POST",
-                headers:{
-                    'Content-Type':"application/json",
-                },
-                body: JSON.stringify(formData)
-            })
-            console.log(response)
-            if(!response.ok){
-                throw new Error('Loging Error')
-            }
-            const data = await response.json();
-            const token = data.data.token;
-            localStorage.setItem("bearer_token", token);
-            console.log("Token received:", token);
-            navigate('/data')
-        }catch (error){
-            setError(error.message)
-        }finally {
-            setLoading(false);
-        }
-        
+export const Login = () => {
+    const [loading, setLoaing] = useState(false);
+    const logger = (email: string, password: string) => {
+        console.log('loggin')
+        console.log(email)
     }
-    return(
-    <div className=" flex items-center justify-center h-[100vh] w-[100vw] flex-col">
-            <Loader
+  return (
+      <div className=" flex items-center justify-center h-[100vh] w-[100vw] flex-col">
+           <Loader loading={loading} />
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+              }}
+        onSubmit={(values) => {
+            // console.log(values);
+            logger('admin@admin.admin','password')
+            // logger(values.email, values.password)
+
+        }}
+      >
+        {({ errors, touched }) => (
+          <Form className="flex flex-col  gap-3 w-64">
+            <h3 className="mb-3 text-2xl font-bold text-gray-900 dark:text-white text-center">
+              Форма входа
+            </h3>
+            <label>
+              <span className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                {" "}
+                Login
+              </span>
+              <Field
+                type="email"
+                name="email"
+                placeholder="email.email@email"
+                className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ${errors.email && touched.email && 'bg-red-200 border-red-500'}`}
+                validate={validateField}
+              />
+              {errors.email && touched.email && <div className="mt-2 text-sm text-red-600 dark:text-red-500">{errors.email}</div>}
+            </label>
+            <label>
+              <span className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Password
+              </span>
+              <Field
+                type="password"
+                name="password"
+                placeholder="*******"
+                className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ${errors.email && touched.email &&  'bg-red-200 border-red-500'}`}
+                validate={validateField}
+              />
+              {errors.password && touched.password && (
+                <div className="mt-2 text-sm text-red-600 dark:text-red-500">{errors.password}</div>
+              )}
+                      </label>
+                      <BlueButton title="Войти" />
+          </Form>
+        )}
+      </Formik>
+      {/* <Loader
                 loading={loading} />
                 <LoaderResult />
                 <h3 className="mb-3 text-2xl font-bold text-gray-900 dark:text-white text-center">
                     Форма входа
                 </h3>
-                {error && <p className="text-red-500">{error}</p>} 
+                {error && <p className="text-red-500">{error}</p>}
                 <form action="#" className="flex flex-col  gap-3 w-64" onSubmit={submit}>
                     <InputField
                         title="Ваш email"
@@ -67,7 +85,7 @@ export const Login =()=>{
                         placeholder="*********"
                         onChange={handleChange} />
                     <BlueButton title="Войти" />
-                </form>
-            </div>
-    )
-}
+                </form> */}
+    </div>
+  );
+};
