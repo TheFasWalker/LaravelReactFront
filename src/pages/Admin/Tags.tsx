081091e74a9bea6fda1423527141a4f53ca7ed88
FAST <krinickij.v.a@gmail.com>
2024-10-23 12:06:31 +0300

    feat: pagination has been moved to a separate component

--- src/pages/Admin/Tags.tsx
+++ src/pages/Admin/Tags.tsx
@@ -7,6 +7,7 @@ import { Loader } from "../../shared/ui/elements/Loader";
 import { useAppDispatch, useAppSelector } from "../../shared/hooks/redux";
 import { getTags } from "../../entities/store/actions/tagsActions";
 import { Field, Form, Formik } from "formik";
+import { Pagination } from "../../shared/admin/HOC/pagination";
 
 export const Tags = () => {
   const dispatch = useAppDispatch();
@@ -35,55 +36,74 @@ export const Tags = () => {
         popupstate={createTagPopupState}
         changePopupState={() => setCreateTagPopupState(!createTagPopupState)}
       >
-      <Formik 
-      initialValues={{
-        title:''
-      }} 
-      onSubmit={()=>console.log('data')}>
-      {({errors,touched})=>(
-        <Form className="flex flex-col  gap-3 w-full">
-          <h1 className=" text-black font-bold  text-3xl">Создать тэг</h1>
-          <label>
-                    <span className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                      Название Тэга
-                    </span>
-                    <Field
-                      type="еуче"
-                      name="title"
-                      className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ${
-                        errors.title && touched.title && "bg-red-200 border-red-500"
-                      }`}
+        <Formik
+          initialValues={{
+            title: ''
+          }}
+          onSubmit={() => console.log('data')}>
+          {({ errors, touched }) => (
+            <Form className="flex flex-col  gap-3 w-full">
+              <h1 className=" text-black font-bold  text-3xl">Создать тэг</h1>
+              <label>
+                <span className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  Название Тэга
+                </span>
+                <Field
+                  type="еуче"
+                  name="title"
+                  className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ${errors.title && touched.title && "bg-red-200 border-red-500"
+                    }`}
 
-                    />
-                    {errors.title && touched.title && (
-                      <div className="mt-2 text-sm text-red-600 dark:text-red-500">
-                        {errors.title}
-                      </div>
-                    )}
-                  </label>
-                  <BlueButton title="Сохранить" type="submit" />
-        </Form>
-      )}
-        
-      </Formik>
+                />
+                {errors.title && touched.title && (
+                  <div className="mt-2 text-sm text-red-600 dark:text-red-500">
+                    {errors.title}
+                  </div>
+                )}
+              </label>
+              <BlueButton title="Сохранить" type="submit" />
+            </Form>
+          )}
+
+        </Formik>
       </PopupLayout>
+
       <PopupLayout
         popupstate={popupState}
         changePopupState={() => setPopupState(!popupState)}
       >
-        <h1 className=" text-black font-bold  text-3xl">Редактирвоание Тэга</h1>
-        <form>
-          <InputField
-            type="text"
-            title="Название Тэга"
-            value="title10"
-            placeholder="Название Тэга"
-            name="title"
-          />
-          <div className=" w-full flex items-center mt-6 justify-center">
-            <BlueButton title="Сохранить изменения" />
-          </div>
-        </form>
+
+
+        <Formik
+          initialValues={{
+            title: ''
+          }}
+          onSubmit={() => console.log('data')}>
+          {({ errors, touched }) => (
+            <Form className="flex flex-col  gap-3 w-full">
+              <h1 className=" text-black font-bold  text-3xl">Редактирвоание Тэга</h1>
+              <label>
+                <span className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  Название Тэга
+                </span>
+                <Field
+                  type="еуче"
+                  name="title"
+                  className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ${errors.title && touched.title && "bg-red-200 border-red-500"
+                    }`}
+
+                />
+                {errors.title && touched.title && (
+                  <div className="mt-2 text-sm text-red-600 dark:text-red-500">
+                    {errors.title}
+                  </div>
+                )}
+              </label>
+              <BlueButton title="Сохранить" type="submit" />
+            </Form>
+          )}
+
+        </Formik>
       </PopupLayout>
 
       <div className="mx-auto max-w-screen-xl px-4 lg:px-12 relative">
@@ -160,106 +180,7 @@ export const Tags = () => {
             )}
           </div>
           {!error && (
-            <nav
-              className="flex flex-col md:flex-row justify-between items-start md:items-center space-y-3 md:space-y-0 p-4"
-              aria-label="Table navigation"
-            >
-              <span className="text-sm font-normal text-gray-500 dark:text-gray-400">
-                Showing
-                <span className="font-semibold text-gray-900 dark:text-white">
-                  1-10
-                </span>
-                of
-                <span className="font-semibold text-gray-900 dark:text-white">
-                  1000
-                </span>
-              </span>
-              <ul className="inline-flex items-stretch -space-x-px">
-                <li>
-                  <a
-                    href="#"
-                    className="flex items-center justify-center h-full py-1.5 px-3 ml-0 text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                  >
-                    <span className="sr-only">Previous</span>
-                    <svg
-                      className="w-5 h-5"
-                      aria-hidden="true"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                  >
-                    1
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                  >
-                    2
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    aria-current="page"
-                    className="flex items-center justify-center text-sm z-10 py-2 px-3 leading-tight text-primary-600 bg-primary-50 border border-primary-300 hover:bg-primary-100 hover:text-primary-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white"
-                  >
-                    3
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                  >
-                    ...
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                  >
-                    100
-                  </a>
-                </li>
-                <li>
-                  <a
-                    href="#"
-                    className="flex items-center justify-center h-full py-1.5 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white"
-                  >
-                    <span className="sr-only">Next</span>
-                    <svg
-                      className="w-5 h-5"
-                      aria-hidden="true"
-                      fill="currentColor"
-                      viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                        clipRule="evenodd"
-                      />
-                    </svg>
-                  </a>
-                </li>
-              </ul>
-            </nav>
+<Pagination/>
           )}
         </div>
       </div>
