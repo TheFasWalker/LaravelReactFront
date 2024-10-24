32d39176c3039be6ebe856cc4d6567d608164676
FAST <krinickij.v.a@gmail.com>
2024-10-24 08:45:41 +0300

    feat created form for edoting category popup

--- src/pages/Admin/Categories.tsx
+++ src/pages/Admin/Categories.tsx
@@ -86,8 +86,41 @@ return(
       popupstate={categoryPopupState}
       changePopupState={() => setCategoryPopupState(false)}>
 
-      <h1 className=" text-black font-bold  text-3xl">Редактирование категории</h1>
-      {popUpData?.title}
+      
+   
+      <Formik
+      initialValues={{
+        title:popUpData?.title
+      }}
+      onSubmit={()=>console.log('sss')}>
+           {({errors,touched})=>(
+         <Form className="flex flex-col  gap-3 w-full">
+          <h1 className=" text-black font-bold  text-3xl">Редактирование категории</h1>
+        <label>
+                <span className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  Название Категории
+                </span>
+                <Field
+                  type="text"
+                  name="title"
+                  placeholder="Заголовк категории"
+                  className={`bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ${errors.title && touched.title && "bg-red-200 border-red-500"
+                    }`}
+
+                />
+                {errors.title && touched.title && (
+                  <div className="mt-2 text-sm text-red-600 dark:text-red-500">
+                    {errors.title}
+                  </div>
+                )}
+                </label>
+            <BlueButton title="Сохранить" type="submit" />
+
+        </Form>
+        )}
+
+      </Formik>
+      
     </PopupLayout>
   <div className="mx-auto max-w-screen-xl px-4 lg:px-12 relative">
     Категории
