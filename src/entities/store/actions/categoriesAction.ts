afee0b0a6c1b58f4e2d32826110dbe1312352a19
krinickij <krinickij.v.a@gmail.com>
2024-10-23 20:52:05 -0400

    feat: edited category actions

--- src/entities/store/actions/categoriesAction.ts
+++ src/entities/store/actions/categoriesAction.ts
@@ -1,5 +1,5 @@
 import { fetchData } from "../../../shared/helpers/fetchData";
-import { categoryData } from "../../types/categories";
+import { Category, categoryData } from "../../types/categories";
 import { categorySlice } from "../slices/categorySlice";
 import { AppDispatch } from "../store";
 
@@ -15,6 +15,62 @@ export const getCategories = (token:string)=> async (dispatch: AppDispatch)=>{
         });
         dispatch(categorySlice.actions.categoriesFetchingSucsess((await responce)))
     }catch(e:any){
-        console.log(e)
+        dispatch(categorySlice.actions.categiriesFetchingError(e.message))
+    }
+}
+export const deleteCategoryById = (token: string, id: string) => async(dispatch: AppDispatch) => {
+    try {
+        dispatch(categorySlice.actions.categoriesFetching());
+        const responce = fetchData(`/category/${id}`, {
+            method: 'DELETE',
+            headers:{
+                Authorization: `Bearer ${token}`,
+                'Content-Type': 'application/json',
+            }
+        })
+        dispatch(categorySlice.actions.categoriesDeleteById(id))
+    } catch (e:any) {
+        dispatch(categorySlice.actions.categiriesFetchingError(e.message))
+    }
+}
+export const createCategoty =(token: string, title: string) => async (dispatch: AppDispatch) => {
+    try {
+        dispatch(categorySlice.actions.categoriesFetching());
+        const data = {
+            title:title
+        }
+        const responce = fetchData<Category>('/category', {
+            method: 'POST',
+            headers:{
+                Authorization: `Bearer ${token}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data)
+        })
+        dispatch(categorySlice.actions.categoriesCreate(await responce))
+
+    } catch (e:any) {
+        dispatch(categorySlice.actions.categiriesFetchingError(e.message))
+    }
+}
+
+export const editCategory= (token: string, title: string, id: number) => async (dispatch: AppDispatch) => {
+    try {
+        dispatch(categorySlice.actions.categoriesFetching());
+        const data = {
+            title:title
+        }
+        const responce = fetchData<Category>(`/category/${id}`, {
+            method: 'PATCH',
+            headers:{
+                Authorization: `Bearer ${token}`,
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(data),
+        })
+        dispatch(categorySlice.actions.categoriesPatch(await responce))
+
+    } catch (e: any) {
+        dispatch(categorySlice.actions.categiriesFetchingError(e.message))
     }
 }
