4ebb9a4ac7551e6584ceca28a1312160919acd69
FAST <krinickij.v.a@gmail.com>
2024-10-24 12:40:42 +0300

    feat: edited post preview

--- src/shared/admin/posts/PreviewPost.tsx
+++ src/shared/admin/posts/PreviewPost.tsx
@@ -5,7 +5,9 @@ interface PostInterface {
     name: string;
     id: number;
     published?:number,
-    onClick?:()=>void;
+    show?:()=>void;
+    edit?:()=>void
+    delete?:()=>void
 }
 
 export const PreviewPost: FC<PostInterface> = ({ name,published, id , onClick}) => {
@@ -68,6 +70,15 @@ export const PreviewPost: FC<PostInterface> = ({ name,published, id , onClick}) 
                         className="z-10 w-44 bg-white rounded divide-y divide-gray-100 shadow dark:bg-gray-700 dark:divide-gray-600 absolute"
                     >
                         <ul className="py-1 text-sm text-gray-700 dark:text-gray-200">
+                        <li>
+                                <button
+                                data-id={id}
+                                  onClick={onClick}
+                                    className=" w-full text-left block py-2 px-4 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                                >
+                                    show
+                                </button>
+                            </li>
                             <li>
                                 <button
                                 data-id={id}
